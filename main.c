31b27e1ace90d31a6dd2e96c6762b2262250de9a
cys <cys01@handong.edu>
2021-04-18 14:19:34 +0900

    modified menuSelect function

--- main.c
+++ main.c
@@ -23,38 +23,6 @@ int main() {
 	while(n != 0) {
 		n = menuSelect();
 
-		switch(n) {
-			case 1:
-				count += createProduct(&p[count]);
-				break;
-			case 2:
-				readList(p, count);
-				break;
-			case 3:
-				if(updateList(p, count) == 1)
-					printf("수정 완료!\n\n");
-				else
-		
-					printf("수정 취소\n\n");
-				break;
-			case 4:
-				if(deleteList(p, count) == 1)
-					printf("삭제 완료!\n\n");
-				else
-					printf("삭제 취소\n\n");
-				break;
-			case 5:
-				saveList("product.txt", p, count);
-				break;
-			case 6:
-				searchName(p, count);
-				break;
-			case 7:
-				searchPrice(p, count);
-				break;
-			case 8:
-				searchStar(p, count);
-		}
 #ifdef _DEBUG
 		if(n == 1 || n == 3 || n == 4)
 			readList(p, count);
--- manager.c
+++ manager.c
@@ -106,10 +106,44 @@ void searchStar(Product *p, int count) {
 	}	
 }
 
-int menuSelect() {
+int menuSelect(Product *p, int num) {
+	static int count = num;
+
 	int n;
 	printf("\n0: 사용종료\n1: 상품추가\n2: 상품조회\n3: 상품수정\n4: 상품삭제\n5: 목록저장\n6: 이름검색\n7: 가격검색\n8: 별점검색\n\n");
 	scanf("%d", &n);
 
+	switch(n) {
+		case 1:
+			count += createProduct(&p[count]);
+			break;
+		case 2:
+			readList(p, count);
+			break;
+		case 3:
+			if(updateList(p, count) == 1)
+				printf("수정 완료!\n\n");
+			else	
+				printf("수정 취소\n\n");
+			break;
+		case 4:
+			if(deleteList(p, count) == 1)
+				printf("삭제 완료!\n\n");
+			else
+				printf("삭제 취소\n\n");
+			break;
+		case 5:
+			saveList("product.txt", p, count);
+			break;
+		case 6:
+			searchName(p, count);
+			break;
+		case 7:
+			searchPrice(p, count);
+			break;
+		case 8:
+			searchStar(p, count);
+	}
+	
 	return n;
 }
